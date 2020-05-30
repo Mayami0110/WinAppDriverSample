65d5ab0bba8f639e5ed60991b0c741e16db2a640
unknown <mayami0110@gmail.com>
2020-05-31 01:19:44 +0530

    first commit

---
+++ src/test/java/com/mytest/NotepadTest.java
@@ -0,0 +1,83 @@
+package com.mytest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.windows.WindowsDriver;
+
+public class NotepadTest {
+
+	public static WindowsDriver driver = null;
+
+	@BeforeClass
+	public void setUp() {
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability("app", "C:\\Windows\\System32\\notepad.exe");
+		cap.setCapability("platformName", "Windows");
+		cap.setCapability("deviceName", "WindowsPC");
+
+		try {
+			driver = new WindowsDriver(new URL("http://127.0.0.1:4723"), cap);
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+		}
+
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+	}
+
+	@AfterMethod
+	public void cleanUp() {
+		driver.quit();
+		setUp();
+	}
+
+	@AfterSuite
+	public void teardown() {
+		driver.quit();
+	}
+
+	@Test
+	public void checkHelpAboutNowTest() {
+
+		driver.findElementByName("Help").click();
+		
+		driver.findElementByName("About Notepad").click();
+		
+		driver.findElementByName("OK").click();
+		
+		
+	}
+	
+	@Test
+	public void sendTextTest()
+	{
+		driver.findElementByName("Text Editor").sendKeys("Hi This is Mayank Mishra");
+		driver.findElementByName("Text Editor").clear();
+
+	}
+
+	@Test
+	public void enterTimeAndDateTest()
+	{
+		driver.findElementByName("Edit").click();
+//		driver.findElementByName("Time/Date").click();
+		driver.findElementByAccessibilityId("26").click();
+		
+		String values = driver.findElementByName("Text Editor").getAttribute("Value.Value");
+
+		System.out.println(values);
+		// driver.findElementByName("Text Editor").clear();
+
+
+	}
+
+}
